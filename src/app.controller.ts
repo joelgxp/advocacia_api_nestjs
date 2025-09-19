92f09b9c2004549d8e3b5b78c41a9165ffe98e43
Joel Souza <joelvieirasouza@gmail.com>
2025-09-19 18:50:52 -0300

    improvements

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -15,7 +15,7 @@ export class AppController {
     return {
       status: 'ok',
       timestamp: new Date().toISOString(),
-      message: 'API de Advocacia funcionando perfeitamente!'
+      message: 'API de Adv funcionando perfeitamente!'
     };
   }
 }
